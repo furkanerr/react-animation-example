224e9be60f6aed1520974681e8fac240fc8f622f
furkaner <furkan08_@hotmail.com>
2022-03-04 21:58:32 +0300

    filter component done

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import FilterStyle from './FilterStyle.css'
+const Filter = ({setFilter,movies,activeGenre,setActiveGenre})=>{
 
-const Filter = ()=>{
+
+    useEffect(()=>{
+        if (activeGenre==0){
+            setFilter(movies)
+        }else{
+            const filteredData = movies?.filter((movie) => movie.genre_ids.includes(activeGenre))
+            setFilter(filteredData);
+            console.log(filteredData)
+        }
+
+
+    },[activeGenre])
+
     return (
         <div className="filterContainer">
-            <button className="AllButton">All</button>
-            <button className="comedyButton">Comedy</button>
-            <button className="ActionButton">Action</button>
-
+            <button onClick={() =>setActiveGenre(0)} className={activeGenre === 0 ? 'active' : ''}>All</button>
+            <button onClick={() =>setActiveGenre(35)} className={activeGenre === 35 ? 'active' : ''}>Comedy</button>
+            <button onClick={() =>setActiveGenre(28)} className={activeGenre === 28 ? 'active' : ''}>Action</button>
         </div>
     );
 };
